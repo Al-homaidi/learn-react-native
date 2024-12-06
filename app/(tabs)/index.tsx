9eed71905dc744ec98fcc1c6f4ba1e08271dc3dc
Al-Homede-1 <155636135+Al-Homede-1@users.noreply.github.com>
2024-12-07 00:13:06 +0300

    a

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -1,3 +1,4 @@
+import React, { useState } from "react";
 import {
   Image,
   StyleSheet,
@@ -12,9 +13,41 @@ import {
   Alert,
 } from "react-native";
 
-
 export default function HomeScreen() {
+  const [text, setText] = useState("shadow");
+
   return (
-    <Text>shadow</Text>
+    <View style={{ backgroundColor: "white", flex: 1, padding: 60 }}>
+      <StatusBar backgroundColor="lightgreen" barStyle="dark-content" />
+      <Text id="main" style={[style.conm, {fontWeight: 'bold'}]}>{text}</Text>
+      <ActivityIndicator size="large" color="red" />
+
+      <Button
+        title="Alert"
+        onPress={() => {
+          Alert.alert("Mohammed Al-homidi", "I am the Eng man yoooooooo", [
+            {
+              text: "Cancel",
+              onPress: () => {
+                setText("Not nice!");
+              },
+            },
+            {
+              text: "Ok",
+              onPress: () => {
+                setText("You're a good man");
+              },
+            },
+          ]);
+        }}
+      />
+    </View>
   );
 }
+
+const style = StyleSheet.create({
+  conm: {
+    backgroundColor: 'red',
+    fontSize: 40,
+  },
+})
