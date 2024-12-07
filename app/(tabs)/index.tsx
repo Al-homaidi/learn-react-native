a7f469c56ba0ebf6d86831a42956947ff541431d
Al-homaidi <alhomaidi505@gmail.com>
2024-12-07 10:05:56 +0300

    adding: new changes

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -14,10 +14,42 @@ import {
   useWindowDimensions,
   SafeAreaView,
 } from "react-native";
-
-
 export default function HomeScreen() {
+  const [text, setText] = useState("shadow");
+
   return (
-    <Text>shadow</Text>
+    <View style={{ backgroundColor: "white", flex: 1, padding: 60 }}>
+      <StatusBar backgroundColor="lightgreen" barStyle="dark-content" />
+      <Text id="main" style={[style.conm, {fontWeight: 'bold'}]}>{text}</Text>
+      <ActivityIndicator size="large" color="red" />
+
+      <Button
+        title="Alert"
+        onPress={() => {
+          Alert.alert("Mohammed Al-homidi", "I am the Eng man yoooooooo", [
+            {
+              text: "Cancel",
+              onPress: () => {
+                setText("Not nice!");
+              },
+            },
+            {
+              text: "Ok",
+              onPress: () => {
+                setText("You're a good man");
+              },
+            },
+          ]);
+        }}
+      />
+    </View>
   );
 }
+
+const style = StyleSheet.create({
+  conm: {
+    backgroundColor: 'red',
+    fontSize: 40,
+  },
+})
+>>>>>>> 9eed71905dc744ec98fcc1c6f4ba1e08271dc3dc
